3291148d483e11a60f95e58922501a088d553cdd
NyaRu-Kiss <nyarusuki@outlook.com>
2026-09-25 18:32:59 +0800

    fix: load business metrics in runnable services

--- flashsale-common/src/main/java/com/flashsale/common/config/CommonWebConfiguration.java
+++ flashsale-common/src/main/java/com/flashsale/common/config/CommonWebConfiguration.java
@@ -1,15 +1,18 @@
 package com.flashsale.common.config;
 
 import com.flashsale.common.api.GlobalExceptionHandler;
+import com.flashsale.common.metrics.BusinessMetricsConfiguration;
 import com.flashsale.common.security.JwtTokenService;
 import com.flashsale.common.trace.TraceIdFilter;
 import java.time.Duration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /** Shared HTTP boundary components explicitly imported by every runnable service. */
 @Configuration(proxyBeanMethods = false)
+@Import(BusinessMetricsConfiguration.class)
 public class CommonWebConfiguration {
     @Bean
     TraceIdFilter traceIdFilter() {
